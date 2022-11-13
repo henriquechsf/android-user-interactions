f966c9d9e39e001285bb85591800f30620015f33
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-11-13 19:09:16 -0300

    Snackbar message

--- app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
@@ -4,21 +4,35 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var showToast: Button
+    lateinit var showSnackbar: Button
+    lateinit var myLayout: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         showToast = findViewById(R.id.button_show_toast)
+        showSnackbar = findViewById(R.id.button_show_snackbar)
+        myLayout = findViewById(R.id.my_layout)
 
         showToast.setOnClickListener {
             Toast
                 .makeText(applicationContext, "This is a Toast Message", Toast.LENGTH_LONG)
                 .show()
+        }
+
+        showSnackbar.setOnClickListener {
+            Snackbar
+                .make(myLayout, "This is a SnackBar Message", Snackbar.LENGTH_INDEFINITE)
+                .setAction("Close", {
+
+                }).show()
         }
     }
 }
