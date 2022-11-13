4d8550f36f4e1c01f0c4ac92026fc0dd9e2f2499
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-11-13 18:56:33 -0300

    Toast message

--- app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
@@ -7,8 +7,18 @@ import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var showToast: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        showToast = findViewById(R.id.button_show_toast)
+
+        showToast.setOnClickListener {
+            Toast
+                .makeText(applicationContext, "This is a Toast Message", Toast.LENGTH_LONG)
+                .show()
+        }
     }
 }
