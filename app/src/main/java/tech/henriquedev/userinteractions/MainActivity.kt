210fe324bf8c033e64969ec6de3d44b010b8be8f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-11-13 19:32:24 -0300

    AlertDialog message

--- app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
+++ app/src/main/java/tech/henriquedev/userinteractions/MainActivity.kt
@@ -4,35 +4,67 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var showToast: Button
-    lateinit var showSnackbar: Button
+    lateinit var showToastButton: Button
+    lateinit var showSnackbarButton: Button
+    lateinit var showDialogButton: Button
     lateinit var myLayout: ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        showToast = findViewById(R.id.button_show_toast)
-        showSnackbar = findViewById(R.id.button_show_snackbar)
+        showToastButton = findViewById(R.id.button_show_toast)
+        showSnackbarButton = findViewById(R.id.button_show_snackbar)
+        showDialogButton = findViewById(R.id.button_show_dialog)
         myLayout = findViewById(R.id.my_layout)
 
-        showToast.setOnClickListener {
-            Toast
-                .makeText(applicationContext, "This is a Toast Message", Toast.LENGTH_LONG)
-                .show()
+        showToastButton.setOnClickListener {
+            showToast()
         }
 
-        showSnackbar.setOnClickListener {
-            Snackbar
-                .make(myLayout, "This is a SnackBar Message", Snackbar.LENGTH_INDEFINITE)
-                .setAction("Close", {
-
-                }).show()
+        showSnackbarButton.setOnClickListener {
+            showSnackbar()
         }
+
+        showDialogButton.setOnClickListener {
+            showAlertDialog()
+        }
+    }
+
+    private fun showToast() {
+        Toast
+            .makeText(applicationContext, "This is a Toast Message", Toast.LENGTH_LONG)
+            .show()
+    }
+
+    private fun showSnackbar() {
+        Snackbar
+            .make(myLayout, "This is a SnackBar Message", Snackbar.LENGTH_INDEFINITE)
+            .setAction("Close") {
+
+            }.show()
+    }
+
+    private fun showAlertDialog() {
+        var alertDialog = AlertDialog.Builder(this@MainActivity)
+        alertDialog
+            .setTitle("Change")
+            .setMessage("Do you want to change the text of the button?")
+            .setIcon(R.drawable.ic_warning)
+            .setCancelable(false)
+            .setNegativeButton("No") { dialogInterface, wich ->
+                dialogInterface.cancel()
+            }
+            .setPositiveButton("Yes") { dialogInterface, wich ->
+                showDialogButton.text = "Alert Dialog"
+            }
+
+        alertDialog.create().show()
     }
 }
